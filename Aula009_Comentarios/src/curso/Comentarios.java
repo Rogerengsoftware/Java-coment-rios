9e1e11610782da88eae5c6449cd318ff0dee2018
Roger Tavares <rogertavaresoperacional@gmail.com>
2024-09-15 12:52:34 -0300

    Java-comentários.

---
+++ Aula009_Comentarios/src/curso/Comentarios.java
@@ -0,0 +1,32 @@
+ package curso;
+
+/**
+ * Programa para exemplificar o uso de comentário
+ * @author roger
+ * @version 1.0
+ * @since 08/2024
+ * 
+ */
+
+public class Comentarios {
+	
+	public static void main(String[] args) {
+		
+		// comentário em bloco
+		
+		/*
+		 * comentário em linha
+		 */
+		
+		//System.out.println("Primeira linha");
+		
+		/*
+		System.out.println("Segunda linha");		
+		System.out.println("Terceira linha");
+		*/
+		
+		System.out.println("Depois do bloco comentado");
+		
+	}
+
+}
